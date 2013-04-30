37798d6256c4cd0531c1fc9c0eacfb8e4a293b1b
Jason Sun <sunapi386@gmail.com>
2013-04-30 13:29:52 -0400

    android: made xml for 3 screens

--- RemoteCtrl/gen/com/example/remotectrl/R.java
+++ RemoteCtrl/gen/com/example/remotectrl/R.java
@@ -24,10 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button_connectivity=0x7f080000;
-        public static final int button_learn=0x7f080002;
-        public static final int button_playback=0x7f080001;
+        public static final int action_settings=0x7f08000a;
+        public static final int button_connectivity=0x7f080005;
+        public static final int button_learn=0x7f080007;
+        public static final int button_playback=0x7f080006;
+        public static final int gridView1=0x7f080009;
+        public static final int learn_button_learn=0x7f080003;
+        public static final int learn_textfield_signal_name=0x7f080002;
+        public static final int learn_textview_choose=0x7f080001;
+        public static final int learn_textview_intro=0x7f080000;
+        public static final int listView_existing_captures=0x7f080004;
+        public static final int textView1=0x7f080008;
     }
     public static final class layout {
         public static final int activity_connect=0x7f030000;
@@ -47,10 +54,13 @@ public final class R {
         public static final int button_connectivity=0x7f050003;
         public static final int button_learn=0x7f050004;
         public static final int button_playback=0x7f050005;
+        public static final int connect_intro=0x7f05000b;
         public static final int hello_world=0x7f050002;
-        public static final int intro_connect=0x7f05000b;
-        public static final int intro_learn=0x7f050009;
-        public static final int intro_play=0x7f05000a;
+        public static final int learn_button_name=0x7f05000d;
+        public static final int learn_hint_signal_name=0x7f05000e;
+        public static final int learn_intro=0x7f050009;
+        public static final int learn_text_choose=0x7f05000c;
+        public static final int play_intro=0x7f05000a;
         public static final int title_activity_connect=0x7f050006;
         public static final int title_activity_learn=0x7f050007;
         public static final int title_activity_play=0x7f050008;
